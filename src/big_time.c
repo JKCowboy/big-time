b2771eea63769d5ec25b445fa0aeda4afce6f8cf
JKCowboy <JKCowboy@users.noreply.github.com>
2015-05-07 18:31:01 -0600

    Animation for hour digits hacked in
    Need to DRY up code but the concept looks like it will work.

--- src/big_time.c
+++ src/big_time.c
@@ -5,8 +5,15 @@
 static Window *s_main_window;
 
 static PropertyAnimation *s_property_animation;
-static PropertyAnimation *s_property_animation_out;
 
+short displayed_hour_digit_1 = -1;
+short displayed_hour_digit_2 = -1;
+short displayed_minute_digit_1 = -1;
+short displayed_minute_digit_2 = -1;
+short new_hour_digit_1 = -1;
+short new_hour_digit_2 = -1;
+short new_minute_digit_1 = -1;
+short new_minute_digit_2 = -1;
 
 // There's only enough memory to load about 6 of 10 required images
 // so we have to swap them in & out... (Ouch!)
@@ -21,7 +28,7 @@ static PropertyAnimation *s_property_animation_out;
 //     2 3
 #define TOTAL_IMAGE_SLOTS 4
 #define NUMBER_OF_IMAGES 10
-#define TRANSITION_MS 900
+#define TRANSITION_MS 300
 #define DIGIT_WIDTH  72
 #define DIGIT_HEIGHT  84
 
@@ -47,11 +54,7 @@ static BitmapLayer *s_image_layers[TOTAL_IMAGE_SLOTS];
 static int s_image_slot_state[TOTAL_IMAGE_SLOTS] = {EMPTY_SLOT, EMPTY_SLOT, EMPTY_SLOT, EMPTY_SLOT};
 
 
-
 static void load_digit_image_into_slot(int slot_number, int digit_value) {
-  
- 
-  
   /*
    * Loads the digit image from the application's resources and
    * displays it on-screen in the correct location.
@@ -62,9 +65,11 @@ static void load_digit_image_into_slot(int slot_number, int digit_value) {
   if ((slot_number < 0) || (slot_number >= TOTAL_IMAGE_SLOTS)) {
     return;
   }
+
   if ((digit_value < 0) || (digit_value > 9)) {
     return;
   }
+
   if (s_image_slot_state[slot_number] != EMPTY_SLOT) {
     return;
   }
@@ -76,118 +81,169 @@ static void load_digit_image_into_slot(int slot_number, int digit_value) {
 #else
   GRect bounds = s_images[slot_number]->bounds;
 #endif
-
-  
-
-  
-  
-  
-  //defaults are for slot0
-  //slide in virtically
-  int startx,starty,endx,endy;
-   startx = 0 ;
-   starty = 0 - DIGIT_HEIGHT;
-   endx = 0;
-   endy = 0;
-  if(slot_number==1){
-    startx = DIGIT_WIDTH;
-    endx = DIGIT_WIDTH;
-  } else if(slot_number==2){
-    starty = DIGIT_HEIGHT * 2;
-    endy= DIGIT_HEIGHT;
-  }  else if(slot_number==3){
-    startx = DIGIT_WIDTH;
-    endx = DIGIT_WIDTH;
-    starty = DIGIT_HEIGHT * 2;
-    endy= DIGIT_HEIGHT;
-  }
-  
-
-  //animated...
-  GRect from_frame = GRect(startx, starty, bounds.size.w, bounds.size.h);
-  GRect to_frame = GRect(endx, endy, bounds.size.w, bounds.size.h);
-
-  BitmapLayer *bitmap_layer = bitmap_layer_create(from_frame);
+  BitmapLayer *bitmap_layer = bitmap_layer_create(GRect((slot_number % 2) * 72, (slot_number / 2) * 84, bounds.size.w, bounds.size.h));
   s_image_layers[slot_number] = bitmap_layer;
   bitmap_layer_set_bitmap(bitmap_layer, s_images[slot_number]);
   Layer *window_layer = window_get_root_layer(s_main_window);
   layer_add_child(window_layer, bitmap_layer_get_layer(bitmap_layer));
-   
-  // Create the animation
-  s_property_animation = property_animation_create_layer_frame(bitmap_layer_get_layer(bitmap_layer), &from_frame, &to_frame);
-  //set duration
-  animation_set_duration((Animation*) s_property_animation, TRANSITION_MS);
-  
-  animation_set_delay((Animation*) s_property_animation, TRANSITION_MS);
-  // Schedule to occur ASAP with default settings
-//   animation_schedule((Animation*) s_property_animation);
-
-  
 }
 
 
 
 static void unload_digit_image_from_slot(int slot_number) {
-  
-  //slide out horizontially...
-  int startx,starty,endx,endy;
-   startx = 0 ;
-   starty = 0 ;
-   endx = 0-DIGIT_HEIGHT;
-   endy = 0;
-  if(slot_number==1){
-    startx = DIGIT_WIDTH;
-    endx = DIGIT_WIDTH*2;
-  } else if(slot_number==2){
-    starty = DIGIT_HEIGHT;
-    endy= DIGIT_HEIGHT;
-  }  else if(slot_number==3){
-    startx = DIGIT_WIDTH;
-    starty = DIGIT_HEIGHT ;
-    endx = DIGIT_WIDTH*2;
-    endy= DIGIT_HEIGHT;
-  }
-  #ifdef PBL_PLATFORM_BASALT
-    GRect bounds = gbitmap_get_bounds(s_images[slot_number]);
-  #else
-    GRect bounds = s_images[slot_number]->bounds;
-  #endif
-  GRect from_frame = GRect(startx, starty, bounds.size.w, bounds.size.h);
-  GRect to_frame = GRect(endx, endy, bounds.size.w, bounds.size.h);
   if (s_image_slot_state[slot_number] != EMPTY_SLOT) {
+    layer_remove_from_parent(bitmap_layer_get_layer(s_image_layers[slot_number]));
+    bitmap_layer_destroy(s_image_layers[slot_number]);
+    gbitmap_destroy(s_images[slot_number]);
+    s_image_slot_state[slot_number] = EMPTY_SLOT;
+  }
+}
+
+
+
+
+
+
+
+
+static void show_hour2(){
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "show_hour2 now %d", new_hour_digit_2);
+
+  //first destroy old image....
+  unload_digit_image_from_slot(1);
+  //create new image
+  s_image_slot_state[1] = new_hour_digit_2; 
+  s_images[1] = gbitmap_create_with_resource(IMAGE_RESOURCE_IDS[new_hour_digit_2]);
+  //animate down
+  #ifdef PBL_PLATFORM_BASALT
+    GRect bounds = gbitmap_get_bounds(s_images[1]);
+  #else
+    GRect bounds = s_images[1]->bounds;
+  #endif
+  GRect from_frame = GRect(DIGIT_WIDTH*2, 0, bounds.size.w, bounds.size.h);
+  GRect to_frame = GRect(DIGIT_WIDTH, 0, bounds.size.w, bounds.size.h);
+
+  BitmapLayer *bitmap_layer = bitmap_layer_create(from_frame);
+  s_image_layers[1] = bitmap_layer;
+  bitmap_layer_set_bitmap(bitmap_layer, s_images[1]);
+  Layer *window_layer = window_get_root_layer(s_main_window);
+  layer_add_child(window_layer, bitmap_layer_get_layer(bitmap_layer));
+   
+  //animate
+  s_property_animation = property_animation_create_layer_frame(bitmap_layer_get_layer(bitmap_layer), &from_frame, &to_frame);
+  animation_set_duration((Animation*) s_property_animation, TRANSITION_MS);
+//   animation_set_handlers((Animation*) s_property_animation, (AnimationHandlers) {
+//     .stopped = (AnimationStoppedHandler) clear_minute1
+//     }, NULL);
+  animation_schedule((Animation*) s_property_animation);
+}
+
+
+
+
+
+ 
+
+static void clear_hour2(){
+
+  if(new_hour_digit_2!=displayed_hour_digit_2){
+    displayed_hour_digit_2=new_hour_digit_2;
+//     this digit changed
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "clear_hour2, digit 2 changing from %d", displayed_hour_digit_2);
+  #ifdef PBL_PLATFORM_BASALT
+    GRect bounds = gbitmap_get_bounds(s_images[1]);
+  #else
+    GRect bounds = s_images[1]->bounds;
+  #endif
+  GRect from_frame = GRect(DIGIT_WIDTH, 0, bounds.size.w, bounds.size.h);
+  GRect to_frame = GRect(DIGIT_HEIGHT*2, 0, bounds.size.w, bounds.size.h);
+  if (s_image_slot_state[1] == EMPTY_SLOT) {
+   load_digit_image_into_slot(1,new_hour_digit_2); 
+  }
     //animate...
-    s_property_animation_out = property_animation_create_layer_frame(bitmap_layer_get_layer(s_image_layers[slot_number]), &from_frame, &to_frame);
-    //set duration
-    animation_set_duration((Animation*) s_property_animation_out, TRANSITION_MS);
-    // Schedule to occur ASAP with default settings
-    animation_schedule((Animation*) s_property_animation_out);
-    
-    //destroy now
-//     layer_remove_from_parent(bitmap_layer_get_layer(s_image_layers[slot_number]));
-//     bitmap_layer_destroy(s_image_layers[slot_number]);
-//     gbitmap_destroy(s_images[slot_number]);
-//     s_image_slot_state[slot_number] = EMPTY_SLOT;
-    
+    s_property_animation = property_animation_create_layer_frame(bitmap_layer_get_layer(s_image_layers[1]), &from_frame, &to_frame);
+    animation_set_duration((Animation*) s_property_animation, TRANSITION_MS);
+    animation_set_handlers((Animation*) s_property_animation, (AnimationHandlers) {
+      .stopped = (AnimationStoppedHandler) show_hour2
+      }, NULL);
+    animation_schedule((Animation*) s_property_animation);
+ 
+  }else{
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "clear_hour2, digit 2 staying %d", displayed_hour_digit_2);
+
+    //no change, check next digit...
   }
 }
 
+static void show_hour1(){
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "show_hour1 now %d", new_hour_digit_1);
+
+  //first destroy old image....
+  unload_digit_image_from_slot(0);
+  //create new image
+  s_image_slot_state[0] = new_hour_digit_1; 
+  s_images[0] = gbitmap_create_with_resource(IMAGE_RESOURCE_IDS[new_hour_digit_1]);
+  //animate down
+  #ifdef PBL_PLATFORM_BASALT
+    GRect bounds = gbitmap_get_bounds(s_images[0]);
+  #else
+    GRect bounds = s_images[0]->bounds;
+  #endif
+  GRect from_frame = GRect(0, 0-DIGIT_HEIGHT, bounds.size.w, bounds.size.h);
+  GRect to_frame = GRect(0, 0, bounds.size.w, bounds.size.h);
+
+  BitmapLayer *bitmap_layer = bitmap_layer_create(from_frame);
+  s_image_layers[0] = bitmap_layer;
+  bitmap_layer_set_bitmap(bitmap_layer, s_images[0]);
+  Layer *window_layer = window_get_root_layer(s_main_window);
+  layer_add_child(window_layer, bitmap_layer_get_layer(bitmap_layer));
+   
+  //animate
+  s_property_animation = property_animation_create_layer_frame(bitmap_layer_get_layer(bitmap_layer), &from_frame, &to_frame);
+  animation_set_duration((Animation*) s_property_animation, TRANSITION_MS);
+  animation_set_handlers((Animation*) s_property_animation, (AnimationHandlers) {
+    .stopped = (AnimationStoppedHandler) clear_hour2
+    }, NULL);
+  animation_schedule((Animation*) s_property_animation);
+}
 
 
-static void display_value(unsigned short value, unsigned short row_number, bool show_first_leading_zero) {
-  value = value % 100; // Maximum of two digits per row.
+static void clear_hour1(){
 
-  // Column order is: | Column 0 | Column 1 |
-  // (We process the columns in reverse order because that makes
-  // extracting the digits from the value easier.)
-  for (int column_number = 1; column_number >= 0; column_number--) {
-    int slot_number = (row_number * 2) + column_number;
-    unload_digit_image_from_slot(slot_number);
-    if (!((value == 0) && (column_number == 0) && !show_first_leading_zero)) {
-      load_digit_image_into_slot(slot_number, value % 10);
-    }
-    value = value / 10;
+  if(new_hour_digit_1!=displayed_hour_digit_1){
+    displayed_hour_digit_1=new_hour_digit_1;
+//     this digit changed
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "clear_hour1, digit 1 changing from %d", displayed_hour_digit_1);
+  #ifdef PBL_PLATFORM_BASALT
+    GRect bounds = gbitmap_get_bounds(s_images[0]);
+  #else
+    GRect bounds = s_images[0]->bounds;
+  #endif
+  GRect from_frame = GRect(0, 0, bounds.size.w, bounds.size.h);
+  GRect to_frame = GRect(0-DIGIT_HEIGHT, 0, bounds.size.w, bounds.size.h);
+  if (s_image_slot_state[0] == EMPTY_SLOT) {
+   load_digit_image_into_slot(0,new_hour_digit_1); 
+  }
+    //animate...
+    s_property_animation = property_animation_create_layer_frame(bitmap_layer_get_layer(s_image_layers[0]), &from_frame, &to_frame);
+    animation_set_duration((Animation*) s_property_animation, TRANSITION_MS);
+    animation_set_handlers((Animation*) s_property_animation, (AnimationHandlers) {
+      .stopped = (AnimationStoppedHandler) show_hour1
+      }, NULL);
+    animation_schedule((Animation*) s_property_animation);
+ 
+  }else{
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "clear_hour1, digit 1 staying %d", displayed_hour_digit_1);
+
+    //no change, check next digit...
   }
 }
+
+  
+
+
+
+
 
 
 
@@ -201,16 +257,43 @@ static unsigned short get_display_hour(unsigned short hour) {
   return display_hour ? display_hour : 12;
 }
 
+ 
+static void update_now_digits(struct tm *tick_time){
+  short hour = get_display_hour(tick_time->tm_hour);
+  short minutes = tick_time->tm_sec;
+  new_hour_digit_1 = hour / 10;
+  new_hour_digit_2 = hour % 10;
+  new_minute_digit_1 = minutes / 10;
+  new_minute_digit_2 = minutes % 10;
+}
 
 
+/*
+* This function sets the current time 
+* digit values and then calls a method that will call others to make the digit changes.
+*/
 static void display_time(struct tm *tick_time) {
-  display_value(get_display_hour(tick_time->tm_hour), 0, false);
+  //first set now digits...
+  update_now_digits(tick_time);
+  //for testing force new digit change everytime...
+  new_hour_digit_1 = (displayed_hour_digit_1 + 1) % 10;
+  new_hour_digit_2 = (displayed_hour_digit_2 + 1)  % 10;
+  new_minute_digit_1 = (displayed_minute_digit_1 + 1) % 10;
+  new_minute_digit_2 = (displayed_minute_digit_2 + 1)  % 10;
+  
+  
+  clear_hour1();
+  
+//   display_value(get_display_hour(tick_time->tm_hour), 0, false);
 //   display_value(tick_time->tm_min, 1, true);
-  display_value(tick_time->tm_sec, 1, true);
+//   display_value(tick_time->tm_sec, 1, true);
 }
 
 
 
+/*
+* This is called by a system timer every minute change. It starts the display change.
+*/
 static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
   int seconds = tick_time->tm_sec;
   if(seconds % 3 == 0){
